8afb2923544d1972d3b7622c4b445e4035f4b704
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2025-10-29 19:54:05 -0300

    HexSpaces

---
+++ main.js
@@ -0,0 +1,185 @@
+//loading webspace
+totalHex = 48;
+hexCount = 0;
+var testMobile = /iPhone|Android|iPad/i.test(navigator.userAgent);
+function loadWebSpace() {
+    commands = "onclick='selecthex(this.id)' onmouseenter='enterhex(this.id)' onmouseleave='leavehex(this.id)' onmousemove='draghex(this.id)' oncontextmenu='hexMenu(event,this.id)'";
+    collums = Math.floor(window.screen.width / 110);
+    lines = 48 / collums;
+    for (l = 0; l < lines; l++) {
+        document.getElementById("webspace").innerHTML += "<div class='hex-grid' id='l" + l + "'></div>";
+        for (c = 0; c < collums; c++) {
+            if (hexCount < totalHex) {
+                document.getElementById("l" + l).innerHTML += "<div class='hex' id='hex" + hexCount + "' " + commands + "><div id='innerhex" + hexCount + "' class='hex-inner'></div></div>";
+                hexCount += 1;
+            }
+        }
+    }
+    hexData();
+}
+
+//loadDatabase
+function hexData() {
+    firebase.database().ref(webspace + "/grid").on('value', function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key; childData = childSnapshot.val();
+            if (childData != "") {
+                document.getElementById("innerhex" + childKey).innerHTML = "<img src='https://www.google.com/s2/favicons?sz=128&domain=" + childData + "'>";
+            }else{
+                document.getElementById("innerhex" + childKey).innerHTML = "";
+            }
+
+        });
+    });
+}
+
+
+//management
+keyPressed = [];
+document.addEventListener('keydown', function (event) {
+    keyPressed.push(event.key);
+    console.log(event.key);
+});
+document.addEventListener('keyup', function (event) {
+    keyPressed = keyPressed.filter(item => item != event.key);
+});
+selected = [];
+
+function openHex(hex) {
+    isopen = false;
+    firebase.database().ref(webspace + "/grid/" + hex).on("value", data => {
+        if (!isopen && data.val() != "") {
+            isopen = true;
+            window.open('https://' + data.val(), '_blank');
+        }
+    })
+}
+
+function selecthex(hex) {
+    if (selected.includes(hex)) {
+        if (keyPressed.includes("Control")) {
+            selected = selected.filter(item => item != hex);
+        } else {
+            openHex(hex.replace('hex', ''))
+        }
+    } else {
+        if (keyPressed.includes("Control")) {
+            selected.push(hex);
+        } else {
+            selected = [hex];
+        }
+    }
+    for (hexs of document.getElementsByClassName('hex')) {
+        if (selected.includes(hexs.id)) {
+            hexs.style.backgroundColor = "lime";
+        } else {
+            hexs.style.backgroundColor = "#454545";
+        }
+    }
+}
+
+function enterhex(hex) {
+    if (!(selected.includes(hex))) {
+        document.getElementById(hex).style.backgroundColor = "white";
+    }
+}
+
+function leavehex(hex) {
+    if (!(selected.includes(hex))) {
+        document.getElementById(hex).style.backgroundColor = "#454545";
+    }
+}
+
+function hexMenu(e,hex){
+    e.preventDefault();
+    if (keyPressed.includes("Control") && !(selected.includes(hex))){
+        selected.push(hex);
+    }else if (!keyPressed.includes("Control")){
+        selected = [hex];
+    }
+    document.getElementById("editBox").style.visibility = "visible";
+    document.getElementById("ehexs").innerHTML = "";
+    document.getElementById("ehexdats").innerHTML = "Hexagonal";
+    for (hexs of selected){
+        document.getElementById("ehexs").innerHTML += hexs + ",";
+    }
+}
+
+function draghex(hex) {
+    //
+}
+
+//functionalities
+function addLink() {
+    link = document.getElementById("linkin").value;
+    added = false;
+    if (link) {
+        firebase.database().ref(webspace + "/grid").on('value', function (snapshot) {
+            if (!added) {
+                snapshot.forEach(function (childSnapshot) {
+                    childKey = childSnapshot.key; childData = childSnapshot.val();
+                    if (childData == "" && !added) {
+                        added = true;
+                        firebase.database().ref(webspace+"/grid/").update({
+                            [childKey]: link.replace('https://','')
+                        });
+                    }
+                });
+            }
+        });
+    }
+}
+
+function goLink() {
+    link = document.getElementById("linkin").value;
+    if (link) {
+        window.open('https://' + link, '_blank');
+    }
+}
+
+function closeEditor(){
+    document.getElementById("editBox").style.visibility = "hidden";
+}
+
+function openAllHex(){
+    opened = false;
+    firebase.database().ref(webspace + "/grid/").on('value', function (snapshot) {
+            if (!opened) {
+                opened = true;
+                snapshot.forEach(function (childSnapshot) {
+                    childKey = childSnapshot.key; childData = childSnapshot.val();
+                    if (childData != "" && selected.includes('hex'+childKey)) {
+                        window.open('https://' + childData, '_blank');
+                    }
+                });
+            }
+        });
+    closeEditor();
+}
+
+function readAllHex(){
+    opened = false;
+    firebase.database().ref(webspace + "/grid/").on('value', function (snapshot) {
+            if (!opened) {
+                opened = true;
+                snapshot.forEach(function (childSnapshot) {
+                    childKey = childSnapshot.key; childData = childSnapshot.val();
+                    if (childData != "" && selected.includes('hex'+childKey)) {
+                        document.getElementById("ehexdats").innerHTML += "<br><a href='"+childData+"'>"+childKey+" | "+childData+"</a>";
+                    }
+                });
+            }
+        });
+}
+
+function deleteHex(){
+    if(confirm("Delete this "+selected.length+" itens?")){
+        for (hexs of selected){
+            item = hexs.replace("hex","");
+            firebase.database().ref(webspace+"/grid/").update({
+                [item]: ""
+            });
+        }
+        closeEditor();
+    }
+}
---
+++ server.js
@@ -0,0 +1,124 @@
+webspace = localStorage.getItem("webspace");
+spacepass = localStorage.getItem("spacepass");
+
+logged = false;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKW9C2RP0tOdcyeOBmsZRii5nzzqBFeow",
+    authDomain: "hexspaces.firebaseapp.com",
+    databaseURL: "https://hexspaces-default-rtdb.firebaseio.com",
+    projectId: "hexspaces",
+    storageBucket: "hexspaces.firebasestorage.app",
+    messagingSenderId: "898528888809",
+    appId: "1:898528888809:web:ff6e271bdefb7589276d78"
+};
+firebase.initializeApp(firebaseConfig);
+
+function logSpace() {
+    spaceinput = document.getElementById("space-code").value;
+    passwordinput = document.getElementById("space-pass").value;
+    if (spaceinput != "" && passwordinput != "") {
+        var spaceref = firebase.database().ref(spaceinput + "/status");
+        var passref = firebase.database().ref(spaceinput + "/password");
+        var isSpaceCreated;
+        var isJoining = false;
+        spaceref.on("value", data => {
+            isSpaceCreated = data.val();
+            if (!isJoining) {
+                isJoining = true;
+                if (isSpaceCreated == "created") {
+                    passref.on("value", data => {
+                        canPass = data.val();
+                        if (canPass == passwordinput) {
+                            localStorage.setItem("webspace", spaceinput);
+                            localStorage.setItem("spacepass", passwordinput);
+                            window.location = "/";
+                        } else {
+                            document.getElementById("login-error").innerHTML = "Incorrect Password";
+                            document.getElementById("space-pass").style.borderColor = "red";
+                        }
+                    })
+                } else {
+                    document.getElementById("login-error").innerHTML = "Incorrect Space-Code";
+                    document.getElementById("space-code").style.borderColor = "red";
+                }
+            }
+        });
+    } else {
+        document.getElementById("login-error").innerHTML = "All the inputs need a value";
+        document.getElementById("space-code").style.borderColor = "yellow";
+        document.getElementById("space-pass").style.borderColor = "yellow";
+    }
+}
+
+function createSpace() {
+    spaceinput = document.getElementById("space-code").value;
+    passwordinput = document.getElementById("space-pass").value;
+    if (spaceinput != "" && passwordinput != "") {
+        var spaceref = firebase.database().ref(spaceinput + "/status");
+        var isSpaceCreated;
+        var isJoining = false;
+        spaceref.on("value", data => {
+            isSpaceCreated = data.val();
+            if (!isJoining) {
+                isJoining = true;
+                if (isSpaceCreated == null) {
+                    firebase.database().ref("/").child(spaceinput).update({
+                        password: passwordinput,
+                        status: "created",
+                        grid:[
+                            "","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+                        ]
+                    });
+                    window.location = "log.html";
+                } else {
+                    document.getElementById("login-error").innerHTML = "This space already exists";
+                    document.getElementById("space-code").style.borderColor = "red";
+                }
+            }
+        });
+    } else {
+        document.getElementById("login-error").innerHTML = "All the inputs need a value";
+        document.getElementById("space-code").style.borderColor = "yellow";
+        document.getElementById("space-pass").style.borderColor = "yellow";
+    }
+}
+
+function loadWebspaceData() {
+    if (webspace != undefined && spacepass != undefined) {
+        var spaceref = firebase.database().ref(webspace + "/status");
+        var passref = firebase.database().ref(webspace + "/password");
+        var isSpaceCreated;
+        var isJoining = false;
+        spaceref.on("value", data => {
+            isSpaceCreated = data.val();
+            if (!isJoining) {
+                isJoining = true;
+                if (isSpaceCreated == "created") {
+                    passref.on("value", data => {
+                        canPass = data.val();
+                        if (canPass == spacepass) {
+                            logged = true;
+                            loadWebSpace();
+                            document.getElementById("space-name").innerHTML = webspace;
+                            console.log("logged: " + logged);
+                        } else {
+                            window.location = "log.html";
+                        }
+                    })
+                } else {
+                    window.location = "log.html";
+                }
+            }
+        });
+    } else {
+        window.location = "log.html";
+    }
+    console.log("logged: " + logged);
+}
+
+function logout(){
+    localStorage.removeItem("webspace");
+    localStorage.removeItem("spacepass");
+    window.location = "/";
+}
